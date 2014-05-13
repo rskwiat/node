ee20a0cd449ad260ebcd63b90aeaf64fbd2d235c
Robert Skwiat <rskwiat@gmail.com>
2014-05-13 17:39:43 -0400

    adding in meadowlark, updating gruntfile, new tour

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -5,45 +5,49 @@ var fortune = require('./lib/fortune.js');
 var app = express();
 var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
 
-    app.engine('handlebars', handlebars.engine);
-    app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
-    app.set('port', 3000);
+app.set('port', 3000);
 
-    app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-    app.use(function(req, res, next){
-        res.locals.showTests = app.get('env') !== 'production' &&
-                req.query.test == '1';
-        next();
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+    req.query.test === '1';
+    next();
+});
+
+app.use(app.router);
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.get('/about', function(req, res){
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
     });
+});
 
-    app.use(app.router);
+app.get('/tours/hood-river', function(req,res){
+    res.render('tours/hood-river');
+});
 
-    app.get('/', function(req, res) {
-        res.render('home');
-    });
+app.get('/tours/oregon-coast', function(req,res){
+    res.render('tours/oregon-coast');
+});
 
-    app.get('/about', function(req, res){
-        res.render('about', {
-                fortune: fortune.getFortune(),
-                pageTestScript: '/qa/tests-about.js'
-         });
-    });
-
-    app.get('/tours/hood-river', function(req,res){
-        res.render('tours/hood-river');
-    });
-
-    app.get('/tours/request-group-rate', function(req, res){
-        res.render('tours/request-group-rate');
-    });
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
+});
 
 
-    app.use(function(req, res, next){
-        res.status(404);
-        res.render('404');
-    });
+app.use(function(req, res, next){
+    res.status(404);
+    res.render('404');
+});
 
 
 
